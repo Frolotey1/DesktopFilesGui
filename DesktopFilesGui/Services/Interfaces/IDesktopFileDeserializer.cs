5ff22e99fa2dd37c57d6666a3ea00dff57ebe5d0
fr1zb3e <168219806+Frolotey1@users.noreply.github.com>
2026-04-07 18:25:13 +0500

    Add files via upload

---
+++ DesktopFilesGui/Services/Interfaces/IDesktopFileDeserializer.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using DesktopFilesGui.Models;
+
+namespace DesktopFilesGui.Services.Interfaces;
+
+public interface IDesktopFileDeserializer
+{
+    Task<DesktopFile> DeserializeAsync(IEnumerable<string> lines);
+    DesktopFile Deserialize(IEnumerable<string> lines);
+}
