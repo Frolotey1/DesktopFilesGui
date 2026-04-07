3bc931708e9b858302b9b03c3189b9bb9f78ca03
fr1zb3e <168219806+Frolotey1@users.noreply.github.com>
2026-04-07 18:33:16 +0500

    Update StringExtensions.cs

--- DesktopFilesGui/Extensions/StringExtensions.cs
+++ DesktopFilesGui/Extensions/StringExtensions.cs
@@ -1,9 +1,10 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace DesktopFilesGui.Extensions;
 
-public static class StringExtensions
-{
+public static class StringExtensions {
     public static string AddPostfix(this string value, string postfix)
     {
         ArgumentNullException.ThrowIfNull(value, postfix);
@@ -11,5 +12,17 @@ public static class StringExtensions
         return value.EndsWith(postfix)
             ? value 
             : value + postfix;
+    }
+    
+    public static IEnumerable<string> ToEnumerableFromDesktopFileArray(this string value)
+    {
+        if (string.IsNullOrWhiteSpace(value))
+            return Enumerable.Empty<string>();
+        
+        return value
+            .TrimEnd(';')
+            .Split(';', StringSplitOptions.RemoveEmptyEntries)
+            .Select(m => m.Trim())
+            .Where(m => !string.IsNullOrWhiteSpace(m));
     }
 }
